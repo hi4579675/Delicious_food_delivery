205fb8a54731a3437d3f8db9bc8f4c7162365ea5
hi4579675 <hi4579675@gmail.com>
2026-04-23 09:17:35 +0900

    fix: 회원 가입 정책 수정 한글/영문/숫자_

--- src/main/java/com/sparta/delivery/user/presentation/dto/SignupRequest.java
+++ src/main/java/com/sparta/delivery/user/presentation/dto/SignupRequest.java
@@ -18,8 +18,8 @@ public record SignupRequest(
         String password,
 
         @NotBlank(message = "이름은 필수입니다.")
-        @Size(min = 4, max = 10, message = "유저네임은 2자 이상 10자 이하입니다.")
-        @Pattern(regexp = "^[a-z0-9]+$", message = "이름은 한글, 영문, 숫자만 가능합니다 (공백/특수문자 불가).")
+        @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하입니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "이름은 한글, 영문, 숫자만 가능합니다 (공백/특수문자 불가).")
         String name,
 
         @Pattern(
