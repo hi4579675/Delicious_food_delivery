0e2f694a975f242f4e4da7a5775010570679722a
sinyeowon <ioohyou@knu.ac.kr>
2026-04-24 14:24:02 +0900

    refactor: PaymentRepository에 existsAnyByOrderIdIncludingDeleted 메서드 추가

--- src/main/java/com/sparta/delivery/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/sparta/delivery/payment/domain/repository/PaymentRepository.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sparta.delivery.payment.domain.entity.Payment;
 
@@ -14,5 +16,8 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     Optional<Payment> findByOrderId(UUID orderId);
 
     Optional<Payment> findByPaymentId(UUID paymentId);
+
+    @Query(value = "select exists (select 1 from p_payment where order_id = :orderId)", nativeQuery = true)
+    boolean existsAnyByOrderIdIncludingDeleted(@Param("orderId") UUID orderId);
 
 }
